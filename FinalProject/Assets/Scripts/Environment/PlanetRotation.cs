9af9e0926fb32107d6eaf283c97c14a3070a3250
Jaime Escobar <48224364+JamesW0lff@users.noreply.github.com>
2020-12-01 22:14:31 -0800

    This is what I added: * Folders to have more organized our project * Textures for Planet Earth * Some materials * Script for planet Rotation * Change the game name to "Moon Cake" (this may change as we decide for thew final name) * Moved some scripts to some folders * Added the space cubemap and skybox

---
+++ FinalProject/Assets/Scripts/Environment/PlanetRotation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotation : MonoBehaviour
+{
+    #region VARIABLES
+    // Create three different rotations, just in case we decide to add more planets, as not all planets rotate the same
+    public float rotationX = 0.0f;
+    public float rotationY = 0.0f;
+    public float rotationZ = 0.0f;
+    #endregion
+
+    #region UPDATE
+    void Update()
+    {
+        // For planet Earth, we will rotate just in the Y axis
+        transform.Rotate(rotationX, rotationY, rotationZ);
+    }
+    #endregion
+}

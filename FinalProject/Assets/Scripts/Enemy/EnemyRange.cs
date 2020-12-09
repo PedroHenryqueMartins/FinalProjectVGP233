f734b6d282703c6eac8154489b07fe7c536cf1a9
Jaime Escobar <48224364+JamesW0lff@users.noreply.github.com>
2020-12-09 00:37:33 -0800

    Enemy animations.

---
+++ FinalProject/Assets/Scripts/Enemy/EnemyRange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyRange : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            this.transform.parent.gameObject.SendMessage("Chase");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        this.transform.parent.gameObject.SendMessage("Patrol");
+    }
+}

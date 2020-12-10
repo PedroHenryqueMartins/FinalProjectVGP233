fe8246950701099a3a8769cb986d78d84cb1df94
Jaime Escobar <48224364+JamesW0lff@users.noreply.github.com>
2020-12-09 16:59:44 -0800

    New terrain added. Enemy script fully modified. Patrol is working properly.

---
+++ FinalProject/Assets/Scripts/Enemy/WaypointManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointManager : MonoBehaviour
+{
+    #region CLASS PATH
+    [Serializable]
+    public class Path
+    {
+        public int Id;
+        public List<Transform> Waypoints;
+    }
+    #endregion
+
+    #region VARIABLES
+    public List<Path> Paths;
+    #endregion
+
+    #region GET PATH
+    public Path GetPath(int id)
+    {
+        return Paths[id];
+    }
+    #endregion
+}

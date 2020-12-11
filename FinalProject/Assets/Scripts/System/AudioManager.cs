f26d15b768855a04cdc29b5a261dd57a0978f531
Jacob <jacobpchacalco@gmail.com>
2020-12-11 12:54:26 -0800

    Created AudioManager class

---
+++ FinalProject/Assets/Scripts/System/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    // Singleton Instance
+    public static AudioManager Instance;
+
+    public AudioSource audioSource;
+
+    [SerializeField]
+    public List<AudioClip> audioClips;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySound(int index)
+    {
+        audioSource.PlayOneShot(audioClips[index]);
+    }
+}

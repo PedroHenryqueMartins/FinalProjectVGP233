49db4b08cfe88878e009337fb9d9e4708a842112
Jacob <jacobpchacalco@gmail.com>
2020-12-11 13:36:59 -0800

    Added changes and comments to AudioManager

--- FinalProject/Assets/Scripts/System/AudioManager.cs
+++ FinalProject/Assets/Scripts/System/AudioManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     // Singleton Instance
-    public static AudioManager Instance;
+    public static AudioManager Instance = null;
 
     public AudioSource audioSource;
 
@@ -31,8 +31,23 @@ public class AudioManager : MonoBehaviour
         audioSource = GetComponent<AudioSource>();
     }
 
+    // Play sound depending on Index
     public void PlaySound(int index)
     {
-        audioSource.PlayOneShot(audioClips[index]);
+        // Check that clip does exist
+        if(audioClips.Count >= index)
+        {
+            audioSource.PlayOneShot(audioClips[index]);
+        }
+    }
+
+    // Play Sound for clicking buttons or UI elements
+    public void PlayClickSound(int index)
+    {
+        // Check that clip does exist
+        if(audioClips.Count >= 2)
+        {
+            audioSource.PlayOneShot(audioClips[1]);
+        }
     }
 }

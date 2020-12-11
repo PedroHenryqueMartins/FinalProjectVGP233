b5038c62362b9c2be769ebf4309cb972106ad74d
Jaime Escobar <48224364+JamesW0lff@users.noreply.github.com>
2020-12-11 00:21:29 -0800

    Added a fifth obstacle course.

--- FinalProject/Assets/Scripts/Platforms/MovePlatform.cs
+++ FinalProject/Assets/Scripts/Platforms/MovePlatform.cs
@@ -6,15 +6,12 @@ public class MovePlatform : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    [Range(1.0f, 10.0f)]
     public float speed;
+
     public GameObject[] waypoints;
     int currentPoint = 0;
     float pointRadius = 1f;
-
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
